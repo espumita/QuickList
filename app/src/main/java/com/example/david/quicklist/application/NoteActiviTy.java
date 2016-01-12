df4be7df0d0a8477f9864894b57467a423266b6b
David Falcón Naranjo <david_el_125@hotmail.com>
2016-01-12 17:04:55 +0000

    Note activity backup

---
+++ app/src/main/java/com/example/david/quicklist/application/NoteActiviTy.java
@@ -0,0 +1,46 @@
+package com.example.david.quicklist.application;
+
+import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+
+import com.example.david.quicklist.R;
+
+import java.util.ArrayList;
+
+public class NoteActiviTy {
+    public NoteActiviTy() {
+        //http://stackoverflow.com/questions/4540754/dynamically-add-elements-to-a-listview-android
+
+        //RelativeLayout relativeLayout = new RelativeLayout(this);
+        //relativeLayout.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT));
+        //relativeLayout.setId(R.id.relativeLayout);
+        //relativeLayout.setBackgroundColor(getResources().getColor(R.color.colorBackGround));
+        //relativeLayout.addView(deployToolBar());
+        //LinearLayout linearLayout = new LinearLayout(this);
+        //RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+        //params.addRule(RelativeLayout.BELOW, R.id.toolbar);
+        //linearLayout.setLayoutParams(params);
+        //linearLayout.setId(R.id.linearLayout);
+        //linearLayout.setOrientation(LinearLayout.VERTICAL);
+        //linearLayout.setPadding(5, 5, 5, 5);
+//
+        //ListView listView = new ListView(this);
+        //listView.setLayoutParams(new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.WRAP_CONTENT));
+        //listView.setAlwaysDrawnWithCacheEnabled(true);
+        //listView.setDividerHeight(1);
+        //listView.setScrollingCacheEnabled(true);
+        //listView.setSmoothScrollbarEnabled(true);
+        //ArrayList<String> arrayList = new ArrayList<String>();
+        //ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, arrayList);
+        //listView.setAdapter(adapter);
+        //for(String annotation : annotationList ){
+        //    arrayList.add(annotation);
+        //    adapter.notifyDataSetChanged();
+        //}
+        //linearLayout.addView(listView);
+        //relativeLayout.addView(linearLayout);
+        //setContentView(relativeLayout);
+    }
+}
